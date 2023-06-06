c9fcdbf614f16e6d67808dce7c6c173fc3c4331f
Log1c <4372712@gmail.com>
2023-06-06 16:08:18 +0300

    Run the application

--- src/main/java/ua/logic/todoList/model/Todo.java
+++ src/main/java/ua/logic/todoList/model/Todo.java
@@ -1,11 +1,8 @@
 package ua.logic.todoList.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import lombok.Data;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "todos")
