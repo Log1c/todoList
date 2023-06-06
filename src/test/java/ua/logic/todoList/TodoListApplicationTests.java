0c0e98dd1675334a3b7351b261c525a7ab04e472
Log1c <4372712@gmail.com>
2023-06-06 12:58:04 +0300

    after using start.spring.io/

---
+++ src/test/java/ua/logic/todoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.logic.todoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
